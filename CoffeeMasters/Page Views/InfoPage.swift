be9f386e3c4e0997fe93d1e04beb9fa77f588c61
Zai Santillan <zaisantillan14@gmail.com>
2023-01-04 18:38:23 +0800

    feat(InfoPage): add WebView

--- CoffeeMasters/Page Views/InfoPage.swift
+++ CoffeeMasters/Page Views/InfoPage.swift
@@ -5,10 +5,34 @@
 
 
 import SwiftUI
+import WebKit
+
+class WebInterfaceDialogs: NSObject, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.body)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.userContentController.add(WebInterfaceDialogs() , name: "alert")
+        return WKWebView(frame: .zero, configuration: config)
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let url = URL(string: "https://firtman.github.io/coffeemasters/webapp")!
+        webView.load(URLRequest(url: url))
+        
+        let name = UserDefaults.standard.string(forKey: "name") ?? ""
+        webView.evaluateJavaScript("injectFormData('\(name)')")
+    }
+}
 
 struct InfoPage: View {
     var body: some View {
-        Text("Info Page")
+        WebView()
     }
 }
 
